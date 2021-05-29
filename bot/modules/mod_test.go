3555381987d4a2ef4d51e86f877787362307dbda
Niyrme <niyrme@niyrme.xyz>
2021-05-29 02:08:18 +0200

    Re-add TestAnswerCommand

--- bot/modules/mod_test.go
+++ bot/modules/mod_test.go
@@ -1,7 +1,7 @@
 package modules_test
 
 import (
-	. "nyooomBot/bot/modules"
+	"nyooomBot/bot/modules"
 	"testing"
 )
 
@@ -36,7 +36,47 @@ func TestContains(t *testing.T) {
 	}
 
 	for _, test := range tests {
-		if output := Contains(test.inputS, test.inputStr); output != test.expected {
+		if output := modules.Contains(test.inputS, test.inputStr); output != test.expected {
+			t.Errorf("Test failed. want: %v; got: %v", test.expected, output)
+		}
+	}
+}
+
+func TestAnswerCommand(t *testing.T) {
+	tests := []struct {
+		cmd      string
+		args     []string
+		expected string
+	}{
+		{ // Test missing argument
+			cmd:      "desc",
+			args:     []string{},
+			expected: modules.ModDesc.How,
+		},
+		{ // Test unknown command
+			cmd:      "desc",
+			args:     []string{"doesNotExist"},
+			expected: "Unknown command doesNotExist",
+		},
+		{ // Test no recursion
+			cmd:      "help",
+			args:     []string{"help"},
+			expected: modules.ModHelp.How,
+		},
+		{ // Test keys
+			cmd:      "h",
+			args:     []string{"ping"},
+			expected: modules.ModPing.How,
+		},
+		{ // Test multiple args
+			cmd:      "help",
+			args:     []string{"description", "ping"},
+			expected: modules.ModDesc.How,
+		},
+	}
+
+	for _, test := range tests {
+		if output := modules.AnswerCommand(test.cmd, test.args); output != test.expected {
 			t.Errorf("Test failed. want: %v; got: %v", test.expected, output)
 		}
 	}
