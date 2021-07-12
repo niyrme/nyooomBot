8207d2a49ef150af273010fec8929fd3d0472304
Niyrme <niyrme@niyrme.xyz>
2021-07-12 21:56:58 +0200

    Change logger padding

--- nyooomBot-Twitch/logging.go
+++ nyooomBot-Twitch/logging.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 )
 
 var (
-	lgrInfo *log.Logger = log.New(os.Stdout, "[INFO] ", log.Ltime|log.Lmsgprefix)
-	lgrErr  *log.Logger = log.New(os.Stdout, "[ERR]  ", log.Ltime|log.Lmsgprefix)
+	lgrInfo *log.Logger = log.New(os.Stdout, fmt.Sprintf("[%-8s] ", "INFO"), log.Ltime|log.Lmsgprefix)
+	lgrErr  *log.Logger = log.New(os.Stdout, fmt.Sprintf("[%-8s] ", "ERR"), log.Ltime|log.Lmsgprefix)
 )
 
 func LogErr(msg string) {
